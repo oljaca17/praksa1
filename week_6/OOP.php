01cafad2af552463d0b5655315c29408e9d8690d
Sasa Oljaca <sasaoljaca85@gmail.com>
2019-09-30 18:27:09 +0200

    ooo

--- week_6/OOP.php
+++ week_6/OOP.php
@@ -1,30 +1,3 @@
 <?php
- 
-class Parfem {
 
-    private $boja = "bezbojan";
-    private $glavnaNota = "jasmin";
-    private $intenzitet = 9;
-    private $godisnjeDoba = "zima";
 
-    private $nazivParfema;
-    private $cijenaParfema;
-
-    
-
-    public function getNazivParfema() {
-        return $this->nazivParfema;
-    }
-
-    public function setNazivParfema($nazivParfema) {
-        $this->nazivParfema = $nazivParfema;
-    }
-
-   
-}
-
-$parfem = new Parfem();
-
-$parfem -> setNazivParfema('Dior');
-
-$parfem -> setVelicina (100);
