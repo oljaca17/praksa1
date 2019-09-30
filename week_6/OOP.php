6609dc1241f90cf08fe32af6918403ffea96b70d
Sasa Oljaca <sasaoljaca85@gmail.com>
2019-09-30 17:46:15 +0200

    funkcija

--- week_6/OOP.php
+++ week_6/OOP.php
@@ -1,1 +1,24 @@
-<h2>this is gregoriantojd</h2>
+<?php
+ 
+class parfem {
+
+    private $boja = "bezbojan";
+    private $glavnaNota = "jasmin";
+    private $intenzitet = 9;
+    private $godisnjeDoba = "zima";
+
+    private $nazivParfema;
+    private $cijenaParfema;
+
+    
+
+    public function getNazivParfema() {
+        return $this->nazivParfema;
+    }
+
+    public function setNazivParfema($nazivParfema) {
+        $this->nazivParfema = $nazivParfema;
+    }
+
+   
+}
