d3c772b70b4a84d4da4a3bd1b6fd17a43b0fc34a
Sasa Oljaca <sasaoljaca85@gmail.com>
2019-09-30 18:40:56 +0200

    new oop

--- week_6/OOP.php
+++ week_6/OOP.php
@@ -1,3 +1,44 @@
 <?php
 
 
+class Pas {
+
+    private $rasa;
+    private $status = 'razigran';
+    private $tezina;
+    private $ime;
+
+
+    public function __construct ($rasa) {
+        $this->rasa = $rasa;
+    }
+
+    public function status () {
+        return $this->status;
+    }
+
+    public function nahrani ($kolicina) {
+        $this->status = 'sit';
+        $this->tezina  += $kolicina/2;
+    }
+
+    public function donesiLoptu () {
+        $this->status = 'razigran';
+        $this->tezina = $this->tezina - 2;
+    }
+
+    public function setTime($ime) {
+        $this->ime = $ime;
+    }
+
+    public function getTime() {
+        return $this->ime;
+    }
+
+    public function govori() {
+        return 'Vau';
+    }
+    public function getStatus() {
+        return $this->status;
+    }
+}
